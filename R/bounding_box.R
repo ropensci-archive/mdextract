6929fe4bff425e98c32fa1e8cef297cc5a6ff1ab
Ted Hart <edmund.m.hart@gmail.com>
2014-09-03 11:10:32 -0700

    finished bounding box fxn

--- R/bounding_box.R
+++ R/bounding_box.R
@@ -1,13 +1,11 @@
 #' @title Automatically calculate a bounding box for your points
 #' @description Take a 2 x n matrix of lat/lon coordinates and convert them to a bounding box
-#' @param points a 2 x n matrix or dataframe of lat/long coordinates
+#' @param points a 2 x n matrix or dataframe of lat/long coordinates in decimal degrees
 #' @return a vector of boundings points as specified by EML - A sequence of: westBoundingCoordinate, eastBoundingCoordinate, northBoundingCoordinate, southBoundingCoordinate 	
 #' @export
 
-bounding_box <- function(points){
+bounding_box <- function(pts){
   
-    coord <- rbind(apply(points,2,min),apply(poinst,2,max))
-    
-    
-  return(spatial)
-}
+    coord <- rbind(apply(pts,2,min),apply(pts,2,max))
+    return(c(coord[,2],coord[,1]))
+    }

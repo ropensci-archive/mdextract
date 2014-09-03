5751daa2f597f19d05671d700cc42e187ed555ac
Jordan S Read <jread@usgs.gov>
2014-09-03 14:59:53 -0500

    simple convex method. need ordering

--- R/points_to_poly.R
+++ R/points_to_poly.R
@@ -2,37 +2,34 @@
 #'
 #'@details creates a automatically generated footprint from a series of points
 #'
-#'@param points a matrix of latitute and longitude points, or a data.frame with 
+#'@param pts a matrix of latitute and longitude points, or a data.frame with 
 #'\code{latitude} and \code{longitude} as column names
-#'@param shape a string specifying the shape of the polygon, defaults as 
-#'\code{'convex'}. Can be either \code{'convex'} or \code{'concave'}
+#'@param method a string specifying the shape of the polygon, defaults as 
+#'\code{'convex'}. Currently \code{'convex'} is the only supported method.
 #'@return a data.frame of latitude and longitude points representing the spatial footprint of the data
 #'@keywords methods, math
 #'@author Jordan S. Read
-#'@import rgdal
+#'@import alphahull
 #'@examples 
 #'file_nm <- system.file(package='mdextract','ext','data_gbif_1.csv')
 #'example_points <- read.table(file=file_nm,header=T,sep=',')
 #'point_matrix <- matrix(as.numeric(c(example_points$decimalLongitude,example_points$decimalLatitude)),
 #'  nrow=nrow(example_points),ncol=2)
-#'points_to_poly(point_matrix)
+#'plot(points_to_poly(point_matrix))
+#'points(point_matrix)
 #'@export
-points_to_poly <- function(points, shape = 'convex'){
+points_to_poly <- function(pts, method = 'convex'){
   
-  if (shape != 'convex' & shape != 'concave'){stop('shape must be either convex or concave')}
+  if (method != 'convex'){stop('method must be either convex or concave')}
   
   # if data.frame, convert to matrix...
-  
-  ll.points <- SpatialPoints(coords= points,
-                             CRS("+init=epsg:2163"))
-  
-  ll.points.tr <- spTransform(ll.points,CRS("+init=epsg:2163"))
-  
-  buf1 <- gBuffer(ll.points.tr, width=90000,byid=T)
-  buf1_union <- gUnionCascaded(buf1) # Take the union -- so this polygon will
-  #contain all your points
-  poly = gBuffer(buf1_union,width=-75000)
-  
+
+  sp <- ashape(x=unique(pts), alpha = 10)
+  x <- c(sp$edges[,3], sp$edges[,5])
+  y <- c(sp$edges[,4], sp$edges[,6])
+  poly = matrix(c(x, y), ncol = 2)
+
+    
   
   return(poly)
 }

c9fcec5f905c58b9adbbd4770439d51617519cb2
Jordan S Read <jread@usgs.gov>
2014-09-03 12:36:09 -0500

    fleshing out buffer function for concave arg

--- R/points_to_poly.R
+++ R/points_to_poly.R
@@ -9,13 +9,28 @@
 #'@return a data.frame of latitude and longitude points representing the spatial footprint of the data
 #'@keywords methods, math
 #'@author Jordan S. Read
+#'@import rgdal
 #'@examples 
-#'example_points <- read.table(file='exampple_data.tsv',header=T,sep='\t')
-# point_matrix <- matrix(as.numeric(c(example_points[, 2],example_points[, 1])),nrow=nrow(example_points),ncol=2)
+#'example_points <- read.table(file='../example_data.tsv',header=T,sep='\t')
+#'point_matrix <- matrix(as.numeric(c(example_points[, 2],example_points[, 1])),nrow=nrow(example_points),ncol=2)
+#'points_to_poly(point_matrix)
 #'@export
 points_to_poly <- function(points, shape = 'convex'){
   
   if (shape != 'convex' & shape != 'concave'){stop('shape must be either convex or concave')}
   
-  return(points)
+  # if data.frame, convert to matrix...
+  
+  ll.points <- SpatialPoints(coords= points,
+                             CRS("+init=epsg:2163"))
+  
+  ll.points.tr <- spTransform(ll.points,CRS("+init=epsg:2163"))
+  
+  buf1 <- gBuffer(ll.points.tr, width=90000,byid=T)
+  buf1_union <- gUnionCascaded(buf1) # Take the union -- so this polygon will
+  #contain all your points
+  poly = gBuffer(buf1_union,width=-75000)
+  
+  
+  return(poly)
 }

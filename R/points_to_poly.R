10dfe7f8e8c51ab1bb6d248140f2eac93f614e3b
Jordan S Read <jread@usgs.gov>
2014-09-03 12:30:29 -0500

    roxygen docs

--- R/points_to_poly.R
+++ R/points_to_poly.R
@@ -9,10 +9,13 @@
 #'@return a data.frame of latitude and longitude points representing the spatial footprint of the data
 #'@keywords methods, math
 #'@author Jordan S. Read
+#'@examples 
+#'example_points <- read.table(file='exampple_data.tsv',header=T,sep='\t')
+# point_matrix <- matrix(as.numeric(c(example_points[, 2],example_points[, 1])),nrow=nrow(example_points),ncol=2)
 #'@export
 points_to_poly <- function(points, shape = 'convex'){
   
   if (shape != 'convex' & shape != 'concave'){stop('shape must be either convex or concave')}
-
   
+  return(points)
 }

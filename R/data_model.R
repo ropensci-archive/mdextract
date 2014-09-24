036bf7f481581ab32b6a607d1ad63e340c381682
emhart <edmund.m.hart@gmail.com>
2014-09-23 21:05:38 -0700

    working on integration with S4 data structures

--- R/data_model.R
+++ R/data_model.R
@@ -5,7 +5,7 @@
 #' @slot taxanomic a list containing the taxanomic coverage metadata
 #'
 
-setClass("coverage", slots = c(name = "character",
+setClass("Coverage", slots = c(name = "character",
                                spatial = "list",
                                temporal = "list",
                                taxanomic = "list"
@@ -16,9 +16,35 @@ setGeneric("coverage",function(name,spatial,temporal,taxanomic) {
   standardGeneric("coverage")
 })
 
-setMethod("coverage", signature("character","list","list","list"),function(name,spatial,temporal,taxanomic){
-  cov <- new(coverage,name = "", spatial = list(),temporal=list(),taxanomic=list())
+### Define constructor
+
+setMethod("coverage", signature(), function(){
+  cov <- new("Coverage",name = covName, spatial = list(),temporal=list(),taxanomic=list())
+  
   return(cov)
 })
+
+
+
+#' list formats
+#' @description list of all object formats registered in the DataONE Object Format Vocabulary.
+#' @param cnode a valid CNode object
+#' @docType methods
+#' @author hart
+#' @return Returns a dataframe of all object formats registered in the DataONE Object Format Vocabulary.
+#' @examples
+#' \dontrun {
+#' cn <- CNode()
+#' listFormats(cn)
+#' }
+#' @export
+setGeneric("listFormats", function(cnode, ...) {
+  standardGeneric("listFormats")
+})
+
+
+#' @rdname listFormats-method
+#' @aliases listFormats
+#' @export
 
 
